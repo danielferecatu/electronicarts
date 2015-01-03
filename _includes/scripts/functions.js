f913a761d3b1bde43ad9b83dff55a3598721e639
Daniel Ferecatu <danvali657@gmail.com>
2015-01-03 11:28:33 +0200

    (c) 2015

--- _includes/scripts/functions.js
+++ _includes/scripts/functions.js
@@ -24,7 +24,7 @@ function date() {
 }
 
 function footer() {
-    document.getElementById("footerscript").innerHTML = "<div id='footer_main_content'><div id='about'><h3>About</h3><a href='http://www.electronicarts.coffeecup.com'>Electronic Arts&trade;</a> is an American company that manufactures and markets video games for computers and game consoles. Most popular games from <a href='http://www.electronicarts.coffeecup.com'>Electronic Arts</a> portofolio are as <a href='http://electronicarts.coffeecup.com/series/fifa/index.html'>FIFA Soccer</a> sports simulators, with <a href='http://electronicarts.coffeecup.com/series/nfs/index.html'>Need for Speed</a>, <a href='http://electronicarts.coffeecup.com/series/medalofhonor/index.html'>Medal of Honor</a>, <a href='http://electronicarts.coffeecup.com/series/sims/index.html'>The Sims</a> and <a href='http://electronicarts.coffeecup.com/series/battlefield/index.html'>Battlefield</a>.</div><div id='links'><h3>Links</h3><nav><a href='http://electronicarts.coffeecup.com/series/battlefield/index.html'>Battlefield</a><a href='http://electronicarts.coffeecup.com/series/nfs/index.html'>Need&nbsp;for&nbsp;Speed</a><a href='http://electronicarts.coffeecup.com/series/burnout/index.html'>Burnout</a><a href='http://electronicarts.coffeecup.com/series/nba/index.html'>NBA&nbsp;Live</a><a href='http://electronicarts.coffeecup.com/series/masseffect/index.html'>Mass&nbsp;Effect</a></nav></div><div id='sitemap'><h3>Sitemap</h3><nav><a href='http://www.electronicarts.coffeecup.com/'>Home</a><a href='http://www.electronicarts.coffeecup.com/ea.html'>Electronic Arts</a><a href='http://www.electronicarts.coffeecup.com/sitemap.htm'>Sitemap</a><a href='http://www.electronicarts.coffeecup.com/contact.html'>Contact</a></nav></div></div><div id='prod'>Design&nbsp;created&nbsp;by&nbsp;<a href='https://plus.google.com/u/0/+DanVali657/posts'>Daniel&nbsp;Ferecatu</a>&nbsp;©&nbsp;2014</div>";
+    document.getElementById("footerscript").innerHTML = "<div id='footer_main_content'><div id='about'><h3>About</h3><a href='http://www.electronicarts.coffeecup.com'>Electronic Arts&trade;</a> is an American company that manufactures and markets video games for computers and game consoles. Most popular games from <a href='http://www.electronicarts.coffeecup.com'>Electronic Arts</a> portofolio are as <a href='http://electronicarts.coffeecup.com/series/fifa/index.html'>FIFA Soccer</a> sports simulators, with <a href='http://electronicarts.coffeecup.com/series/nfs/index.html'>Need for Speed</a>, <a href='http://electronicarts.coffeecup.com/series/medalofhonor/index.html'>Medal of Honor</a>, <a href='http://electronicarts.coffeecup.com/series/sims/index.html'>The Sims</a> and <a href='http://electronicarts.coffeecup.com/series/battlefield/index.html'>Battlefield</a>.</div><div id='links'><h3>Links</h3><nav><a href='http://electronicarts.coffeecup.com/series/battlefield/index.html'>Battlefield</a><a href='http://electronicarts.coffeecup.com/series/nfs/index.html'>Need&nbsp;for&nbsp;Speed</a><a href='http://electronicarts.coffeecup.com/series/burnout/index.html'>Burnout</a><a href='http://electronicarts.coffeecup.com/series/nba/index.html'>NBA&nbsp;Live</a><a href='http://electronicarts.coffeecup.com/series/masseffect/index.html'>Mass&nbsp;Effect</a></nav></div><div id='sitemap'><h3>Sitemap</h3><nav><a href='http://www.electronicarts.coffeecup.com/'>Home</a><a href='http://www.electronicarts.coffeecup.com/ea.html'>Electronic Arts</a><a href='http://www.electronicarts.coffeecup.com/sitemap.htm'>Sitemap</a><a href='http://www.electronicarts.coffeecup.com/contact.html'>Contact</a></nav></div></div><div id='prod'>Design&nbsp;created&nbsp;by&nbsp;<a href='https://plus.google.com/u/0/+DanVali657/posts'>Daniel&nbsp;Ferecatu</a>&nbsp;©&nbsp;2015</div>";
 }
 
 function menu() {
@@ -65,15 +65,3 @@ function handleScroll() {
         }
     });
 }
-
-
-// // Google Analystics
-
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-49308473-2', 'coffeecup.com');
-// ga('send', 'pageview');
-
